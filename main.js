7aaa23b03dbc2bca7bf64d772352bf525a50b25f
Michael Musil <infi.pc@gmail.com>
2022-01-06 14:07:08 +0100

    sales data

--- main.js
+++ main.js
@@ -7,14 +7,8 @@ const {
 
 Apify.main(async () => {
   const { slugs = [], codes = [] } = (await Apify.getInput()) ?? {
-    slugs: [
-      // "air-jordan-max-aura-black"
-    ],
-    codes: [
-      "AT8240-103",
-      // 'VN0A4U39ZPM',
-      // 'FW8978'
-    ],
+    slugs: ["air-jordan-max-aura-black"],
+    codes: ["AT8240-103", "VN0A4U39ZPM", "FW8978"],
   };
 
   console.log(`slugs: ${slugs}`);
@@ -33,7 +27,6 @@ Apify.main(async () => {
 
   // load cachedUrl LUT code -> url
   for (const code of codes) {
-    // `CT8013-170`
     const cachedSlug = await codesMapKVStore.getValue(code);
     if (cachedSlug) {
       log.info(
@@ -47,7 +40,7 @@ Apify.main(async () => {
     } else {
       log.info(`Code ${code} not found in the store, adding`);
       const url =
-        "https://xw7sbct9v6-1.algolianet.com/1/indexes/products/query?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%203.32.1&x-algolia-application-id=XW7SBCT9V6&x-algolia-api-key=6b5e76b49705eb9f51a06d3c82f7acee";
+        "https://xw7sbct9v6-1.algolianet.com/1/indexes/products/query?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%203.32.1&x-algolia-application-id=XW7SBCT9V6&x-algolia-api-key=6b5e76b49705eb9f51a06d3c82f7acee&x-xcode="+ code;
       const headers = {
         // 'user-agent': userAgent,
         "sec-fetch-dest": "none",
@@ -55,6 +48,8 @@ Apify.main(async () => {
         "sec-fetch-site": "cross-site",
         "sec-fetch-mode": "cors",
         "accept-language": "en-US",
+        // Just for cachebusting apify, Apify can't take body in request, so i have to put the unique code somewhere
+        // "X-code": code,
       };
 
       const body = JSON.stringify({
@@ -63,8 +58,12 @@ Apify.main(async () => {
       const method = "POST";
       await requestQueue.addRequest({
         url,
-        userData: { type: "CODE", code, headers, body, method },
+        method,
+        headers,
+        payload: body,
+        userData: { type: "CODE", code },
       });
+      log.info(`Code ${code} not found in the store, added to queue`);
     }
   }
 
@@ -82,15 +81,20 @@ Apify.main(async () => {
       const { request, session } = context;
       const {
         url,
-        userData: { type, headers, body, method },
+        headers, 
+        method,
+        payload,
+        userData: { type },
       } = request;
+
+      log.info(`Processing ${method || "GET"} ${context.request.url}`);
 
       const finalRequest = { url: request.url };
       if (headers) {
         finalRequest.headers = headers;
       }
-      if (body) {
-        finalRequest.body = body;
+      if (payload) {
+        finalRequest.payload = payload;
       }
       if (method) {
         finalRequest.method = method;
@@ -100,17 +104,17 @@ Apify.main(async () => {
 
       switch (response.statusCode) {
         case 404:
-          log.info("404 » skipping", { url });
+          log.error("404 » skipping", { url });
           return;
         case 403:
-          log.info("403 (blocked) » retiring session and aborting", { url });
+          log.error("403 (blocked) » retiring session and aborting", { url });
           session.retire();
           throw "403"; // Not throwing error, no need for call-stack
         case 200:
           log.info("200 (ok) » continuing", { url });
           break;
         default:
-          log.info("Unhandled status » retiring session and aborting", {
+          log.error("Unhandled status » retiring session and aborting", {
             url,
             status: response.statusCode,
           });
@@ -138,10 +142,11 @@ Apify.main(async () => {
               url: makeDetailUrl(mathing[0].url),
               userData: { type: "DETAIL", code: mathing[0].style_id },
             });
+            log.info("Adding to queue", { url: makeDetailUrl(mathing[0].url) });
             await codesMapKVStore.setValue(mathing[0].style_id, mathing[0].url);
           } else {
-            log.info(`Code ${request.userData.code} not found, skipping`);
-            log.info(
+            log.error(`Code ${request.userData.code} not found, skipping`);
+            log.error(
               `available codes: ${hits
                 .map(({ style_id }) => style_id)
                 .join(", ")}`
@@ -149,10 +154,15 @@ Apify.main(async () => {
           }
           return;
         case "DETAIL":
+          const parsedData = JSON.parse(response.body)
+          const id = parsedData.Product.id
+          const sales = await Apify.utils.requestAsBrowser({url: `https://stockx.com/api/products/${id}/activity?limit=100&page=1&sort=createdAt&order=DESC&state=480&currency=EUR&country=CZ`});
+          
           await Apify.pushData({
             "#success": true,
             url: request.url,
-            data: JSON.parse(response.body),
+            sales: sales.statusCode == 200 ? JSON.parse(sales.body) : String(sales.body),
+            data: parsedData,
           });
           return;
         default:
